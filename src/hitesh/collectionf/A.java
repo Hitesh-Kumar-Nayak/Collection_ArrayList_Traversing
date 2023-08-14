86dc6df121d4c8e407dffb6a22456170fcd4c2fa
Hitesh IT <hiteshkumarnayak796@gmail.com>
2023-08-14 16:23:09 +0530

    initial commit

---
+++ src/hitesh/collectionf/A.java
@@ -0,0 +1,64 @@
+package hitesh.collectionf;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.function.Consumer;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);//Insertion order is maintained
+		al.add(20);//Duplicate is Allowed
+		al.add(30);//null value is allowed
+		al.add(40);//internal ds is growable array
+		al.add(50);//default sizee is 10
+		al.add(60);//it is not syncronized
+		al.add(70);//both Homogeneous & hetrogeneous allowed
+		al.add(80);
+		al.add(90);
+		al.add(100);
+		al.add(10);
+		
+		System.out.println(al);
+		System.out.println(al.indexOf(10));
+		System.out.println(al.lastIndexOf(10));
+		System.out.println(al.get(4));
+		al.add(1, 110);
+		System.out.println(al);
+		al.set(0, 120);
+		System.out.println(al);
+		al.add(0, 150);
+		
+		
+		System.out.println("=====For loop");
+		for(int i =0;i<al.size();i++) {
+			}
+		System.out.println(al);
+		
+		System.out.println("=====for Each===== ");
+		for(Integer i :al) {
+			System.out.println(i);
+		}
+		
+		System.out.println("====Using Iterator====");
+		
+		Iterator<Integer> iterator = al.iterator();
+				while(iterator.hasNext()) {
+					System.out.println(iterator.next());
+				}
+				
+				System.out.println("====Using ListIterator=====");
+				
+				ListIterator<Integer> listIterator = al.listIterator();
+				while(listIterator.hasNext()){
+					System.out.println(listIterator.next());
+					
+				}
+				
+				
+	}
+
+}
